03fcab814c5658c9dd093c2560064ee68deb2ba6
dgarrido-laberit <dgarrido@laberit.com>
2022-10-26 09:32:17 +0200

    Capitulo 4.Add services - Antes de Observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,22 +10,28 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  // heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-  constructor() { }
-
+  constructor(private heroService: HeroService) { }
+  
   ngOnInit(): void {
+    this.getHeroes();
   }
-
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   } 
-
+  
   onChangeTextParent (text: string, hero : Hero): void {
     console.log("P: " + text);
     hero.name = text;
+  }
+  
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 
 }
