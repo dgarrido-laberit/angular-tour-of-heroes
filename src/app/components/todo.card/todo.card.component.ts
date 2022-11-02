cd7b397e22f732d2e853a6128e908f023a829715
dgarrido-laberit <dgarrido@laberit.com>
2022-11-02 15:03:25 +0100

    Otro pequeño avance.

--- src/app/components/todo.card/todo.card.component.ts
+++ src/app/components/todo.card/todo.card.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 import { todoTypes, todoTypeNames } from 'src/app/constants/todo.constants';
+import { Todo } from 'src/app/models/todo.model';
 
 @Component({
   selector: 'app-todo-card',
@@ -9,8 +10,8 @@ import { todoTypes, todoTypeNames } from 'src/app/constants/todo.constants';
 })
 export class TodoTypeComponent implements OnInit {
 
-  @Input() userId: number | undefined;
-  @Output() userIdChange = new EventEmitter<number>();
+  @Input() todo: Todo | undefined;
+  // @Output() userIdChange = new EventEmitter<Todo>();
 
   todoTypes = todoTypes;
   todoTypeNames = todoTypeNames;
